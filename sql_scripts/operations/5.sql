f78dd8c38ab157f0bed54fe229f707f0c0491205
Jakub Plona <plonajakub@gmail.com>
2019-06-07 23:08:52 +0200

    5a finished

--- sql_scripts/operations/5.sql
+++
@@ -1,5 +0,0 @@
-SELECT r.recipe_id, r.recipe_name, i.ingredient_id, i.ingredient_name, i.quantity AS INGREDIENT_QUANTITY_IN_STOCK, ifr.ingredient_quantity as REQUIRED_INGEREDIENT_QUANTITY
-FROM recipes r INNER JOIN ingredients_for_recipe ifr ON r.recipe_id = ifr.recipe_id
-               INNER JOIN ingredients i ON ifr.ingredient_id = i.ingredient_id
-WHERE i.ingredient_name IN ('OCTINOXATE', 'Propranolol Hydrochloride', 'Guaifenesin')
-ORDER BY r.recipe_name;
