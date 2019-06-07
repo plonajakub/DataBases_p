9066a2ba966267270d4536dd2e5cbb85f1482475
Jakub Plona <plonajakub@gmail.com>
2019-06-07 19:35:57 +0200

    tables creation script updated

--- sql_scripts/table/db_tables_creation.sql
+++ sql_scripts/table/db_tables_creation.sql
@@ -2,9 +2,9 @@
 
 --1)
 CREATE TABLE Suppliers (
-supplier_id NUMBER(4) PRIMARY KEY ,
+supplier_id NUMBER(4) PRIMARY KEY,
 supplier_name varchar2(50) NOT NULL,
-phone_number varchar2(14) NOT NULL UNIQUE,
+phone_number varchar2(14) NOT NULL UNIQUE CHECK(regexp_like(phone_number, '^\+?\d{9,}$')),
 address varchar2(50)
 );
 
@@ -12,7 +12,7 @@ address varchar2(50)
 CREATE TABLE Ingredients (
 ingredient_id NUMBER(5) PRIMARY KEY,
 ingredient_name varchar2(100) NOT NULL,
-quantity NUMBER(7) DEFAULT 0 CHECK (quantity>=0),
+quantity NUMBER(7) DEFAULT 0 NOT NULL CHECK (quantity >= 0),
 notes varchar2(100),
 supplier_id NUMBER(4),
 CONSTRAINT supplier_id_fk FOREIGN KEY (supplier_id)
@@ -29,15 +29,16 @@ manufacturer_name varchar2(50)
 
 --4)
 CREATE TABLE Employees (
-employee_id NUMBER(5) PRIMARY KEY ,
+employee_id NUMBER(5) PRIMARY KEY,
 first_name varchar2(50) NOT NULL,
 last_name varchar2(50) NOT NULL,
-salary NUMBER(5) CHECK (salary>=0)
+salary NUMBER(5) CHECK (salary >= 0)
 );
 
 --5)
 CREATE TABLE Data_types (
-data_type varchar2(30) PRIMARY KEY
+data_type varchar2(30) PRIMARY KEY,
+unit varchar2(15)
 );
 
 --6)
@@ -50,31 +51,33 @@ description varchar2(1000) NOT NULL
 --7)
 /* Wykaz składników potrzebnych do wykonania receptury */
 CREATE TABLE Ingredients_for_recipe (
-recipe_id NUMBER(5)  NOT NULL ,
-ingredient_id NUMBER(5) NOT NULL , 
+recipe_id NUMBER(5),
+ingredient_id NUMBER(5), 
 ingredient_quantity NUMBER(7) NOT NULL CHECK (ingredient_quantity > 0),
 CONSTRAINT recipe_id_fk FOREIGN KEY (recipe_id)
 REFERENCES Recipes(recipe_id),
-CONSTRAINT ingredient_id FOREIGN KEY (ingredient_id)
-REFERENCES Ingredients(ingredient_id)
+CONSTRAINT ingredient_id_fk FOREIGN KEY (ingredient_id)
+REFERENCES Ingredients(ingredient_id),
+CONSTRAINT ingredients_for_recipe_pk PRIMARY KEY (recipe_id, ingredient_id)
 );
 
 --8)
 /* Wykaz narzędzi potrzebnych do wykonania receptury */
 CREATE TABLE Tools_for_recipe (
-recipe_id NUMBER(5) NOT NULL ,
-tool_id NUMBER(5) NOT NULL, 
+recipe_id NUMBER(5),
+tool_id NUMBER(5), 
 CONSTRAINT tool_id_fk FOREIGN KEY (tool_id) 
 REFERENCES Tools(tool_id),
 CONSTRAINT recipe_id_fk2 FOREIGN KEY (recipe_id) 
-REFERENCES Recipes(recipe_id)
+REFERENCES Recipes(recipe_id),
+CONSTRAINT tools_for_recipe_pk PRIMARY KEY (recipe_id, tool_id)
 );
 
 --9)
 CREATE TABLE Projects (
 project_id NUMBER(5) PRIMARY KEY,
 project_name varchar2(30) UNIQUE,
-manager_id NUMBER(5) NOT NULL ,
+manager_id NUMBER(5) NOT NULL,
 description varchar2(1000),
 CONSTRAINT manager_id_fk FOREIGN KEY (manager_id) 
 REFERENCES Employees(employee_id)
@@ -83,22 +86,22 @@ REFERENCES Employees(employee_id)
 --10)
 /* Wykaz pracowników uprawnionych do wglądu w projekt */
 CREATE TABLE Authorized_employees (
-project_id NUMBER(5) NOT NULL, 
-employee_id NUMBER(5) NOT NULL,
+project_id NUMBER(5), 
+employee_id NUMBER(5),
 CONSTRAINT employee_id_fk FOREIGN KEY (employee_id) 
 REFERENCES Employees(employee_id),
 CONSTRAINT project_id_fk FOREIGN KEY (project_id)
-REFERENCES Projects(project_id) 
+REFERENCES Projects(project_id),
+CONSTRAINT authorized_employees_pk PRIMARY KEY (project_id, employee_id)
 );
 
 --11)
 CREATE TABLE Processes (
-process_id NUMBER(5) PRIMARY KEY ,
-project_id NUMBER(5) NOT NULL ,
-recipe_id NUMBER(5) NOT NULL ,
-employee_id NUMBER(5) NOT NULL ,
-status varchar2(12) NOT NULL 
-CHECK (status IN ('in_progress', 'planned', 'success', 'fail')),
+process_id NUMBER(5) PRIMARY KEY,
+project_id NUMBER(5) NOT NULL,
+recipe_id NUMBER(5) NOT NULL,
+employee_id NUMBER(5) NOT NULL,
+status varchar2(11) DEFAULT 'planned' NOT NULL CHECK (status IN ('in_progress', 'planned', 'success', 'fail')),
 start_date timestamp,
 end_date timestamp,
 notes varchar2(1000),
@@ -113,22 +116,24 @@ REFERENCES Employees(employee_id)
 --12)
 /*Wykaz danych procesu*/
 CREATE TABLE Process_data (
-process_id NUMBER(5) NOT NULL, 
-data_type varchar2(30) NOT NULL,
-data_value varchar2(30) DEFAULT '-' NOT NULL,
+process_id NUMBER(5), 
+data_type varchar2(30),
+data_value NUMBER(20, 5) DEFAULT 0 NOT NULL,
 CONSTRAINT process_id_fk FOREIGN KEY (process_id)
 REFERENCES Processes(process_id),
 CONSTRAINT data_type_fk FOREIGN KEY (data_type) 
-REFERENCES Data_types(data_type)
+REFERENCES Data_types(data_type),
+CONSTRAINT process_data_pk PRIMARY KEY (process_id, data_type)
 );
 
 --13)
 /*Wykaz aktualnie używanych narzędzi*/
 CREATE TABLE Tools_in_use (
-process_id NUMBER(5) NOT NULL ,
-tool_id NUMBER(5) NOT NULL, 
+process_id NUMBER(5),
+tool_id NUMBER(5), 
 CONSTRAINT tool_id_fk2 FOREIGN KEY (tool_id)
 REFERENCES Tools(tool_id),
 CONSTRAINT process_id_fk2 FOREIGN KEY (process_id)
-REFERENCES Processes(process_id)
+REFERENCES Processes(process_id),
+CONSTRAINT tools_in_use_pk PRIMARY KEY (process_id, tool_id)
 );
