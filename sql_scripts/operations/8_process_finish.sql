217bc72916802b3031be8227b87e21b43b3a1a7a
Jakub Plona <plonajakub@gmail.com>
2019-06-08 23:36:29 +0200

    timestamp fix

--- sql_scripts/operations/8_process_finish.sql
+++ sql_scripts/operations/8_process_finish.sql
@@ -1,7 +1,7 @@
 --A i B--
 SET TRANSACTION NAME 'process_finish'
 UPDATE Processes
-SET status = 'success', end_date = CURRENT_TIMESTAMP()
+SET status = 'success', end_date = CURRENT_TIMESTAMP(0)
 WHERE process_id = __id__;
 INSERT INTO process_data (process_id, data_type, data_value)
 VALUES (__process_id__, __data_type__, __value__);
